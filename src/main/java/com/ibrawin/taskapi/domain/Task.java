f9d5bf4e2f7bf38046156d14285cc147461ac834
Ibrahim Ayanbunmi <ibrahim-ayanbunmi@live.co.uk>
2026-07-13 02:47:47 +0100

    add task entity

---
+++ src/main/java/com/ibrawin/taskapi/domain/Task.java
@@ -0,0 +1,35 @@
+package com.ibrawin.taskapi.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Task")
+@Data
+public class Task {
+
+    @Id
+    @Column(name = "task_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "completed")
+    private boolean completed;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
